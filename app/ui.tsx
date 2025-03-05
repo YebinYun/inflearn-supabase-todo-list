6d4f609646eb7690f8ef494e06226aa8696a061f
yebin <yebind3308@gmail.com>
2025-03-06 00:38:05 +0900

    feat:Part 2. UI 작업

---
+++ app/ui.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button, Input } from "@material-tailwind/react";
+import Todo from "components/todo";
+
+export default function UI() {
+  return (
+    <div className="w-2/3 mx-auto flex flex-col items-center py-10 gap-2">
+      <h1 className="text-xl">TODO LIST</h1>
+
+      <Input
+        label="Search TODO"
+        placeholder="Search TODO"
+        icon={<i className="fas fa-search" />}
+        onPointerEnterCapture={() => {}}
+        onPointerLeaveCapture={() => {}}
+        crossOrigin=""
+      />
+
+      <Todo />
+
+      <Button
+        onClick={() => {}}
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
+        <i className="fas fa-plus mr-2" />
+        ADD TODO
+      </Button>
+    </div>
+  );
+}
